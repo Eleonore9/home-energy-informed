d6a37dac0cb2f9df0f26e0ee5f812ea2a3d799c2
Matt Mottram <mjmottram@gmail.com>
2016-02-06 23:15:04 +0000

    working google charts

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,49 +1,46 @@
-$( document ).ready(function() {
+console.log( "Hello!" )
 
-  console.log( "Hello!" )
+// Load the Visualization API and the piechart package.
+//google.charts.load('visualization', '1.0', {'packages':['corechart']});
+google.charts.load('current', {'packages': ['corechart']});
 
-  $.getJSON( "/dashboard/data", function() {
-  })
-  .done(function( data ) {
-    console.log( "success" );
-    var gasReading = data;
-    
-    // Load the Visualization API and the piechart package.
-      google.load('visualization', '1.0', {'packages':['corechart']});
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
 
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(drawChart);
-
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-    drawChart();
-
-    
-  })
-  
-});
-
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+//drawChart();
 function drawChart() {
-
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
-
-        // Set chart options
-        var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':400,
-                       'height':300};
-
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
+    console.log("draw...");
+    
+    $.getJSON( "/dashboard/data", function() {
+    })
+        .done(function( data ) {
+            var gasReading = data;
+            
+            // Create the data table.
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Topping');
+            data.addColumn('number', 'Slices');
+            data.addRows([
+                ['Mushrooms', 3],
+                ['Onions', 1],
+                ['Olives', 1],
+                ['Zucchini', 1],
+                ['Pepperoni', 2]
+            ]);
+            
+            // Set chart options
+            var options = {'title':'How Much Pizza I Ate Last Night',
+                           'width':400,
+                           'height':300};
+            console.log( "success" );
+            
+            // Instantiate and draw our chart, passing in some options.
+            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+            console.log( "success" );
+            chart.draw(data, options);
+            console.log("done");
+        });
+}
