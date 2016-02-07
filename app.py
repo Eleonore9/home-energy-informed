62f2e3eb835aaaa312531c240362d9a4c6ab9db3
Eleonore9 <eleonore.mayola@ymail.com>
2016-02-07 12:37:53 +0000

    Add filter per building type

--- app.py
+++ app.py
@@ -30,6 +30,12 @@ def elec_data():
     elec_readings = dr.read_elec_data(101, 101, 4)
     return elec_readings
 
+@app.route('/dashboard/gas_data/<int:type>')
+def data_readings_filtered(type):
+    gas_readings = dr.read_gas_data(type, 101, 4)
+    return gas_readings
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
